8104a0c6db1c879312d5040ecd7022d615141813
guff192 <kochkons1@ya.ru>
2023-10-17 03:47:04 +0400

    Updated main test

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,4 +6,4 @@ def test_read_main(client: TestClient):
     response = client.get("/")
     assert response.status_code == 200
     assert 'text/html' in response.headers.get('content-type', '')
-    assert ">Simple Site</h1>" in response.text
+    assert ">Welcome</h1>" in response.text
